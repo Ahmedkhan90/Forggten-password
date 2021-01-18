a4a8cb563417d2d5b60bed249e368d1a19642d71
Ahmedkhan90 <dxahmedkhan@gmail.com>
2021-01-18 18:19:41 +0500

    Changes

--- public/app.js
+++ public/app.js
@@ -9,24 +9,24 @@ function signup() {
     // var gender = (document.getElementById("gender").value);
 
 
-    // axios({
-    //     method: 'post',
-    //     url: "http://localhost:3000/signup",
-    //     data: {
-    //         uname: uname,
-    //         email: email,
-    //         password: password,
-    //         phone: phone,
-    //         gender :gender
-    //     }
-    // })
-    // .then(function (response) {
-    //     console.log(response.data.message);
-    //     alert(response.data.message);
-    // })
-    // .catch(function (error) {
-    //     console.log(error);
-    // });
+    axios({
+        method: 'post',
+        url: "http://localhost:3000/signup",
+        data: {
+            uname: uname,
+            email: email,
+            password: password,
+            phone: phone,
+            gender :gender
+        }
+    })
+    .then(function (response) {
+        console.log(response.data.message);
+        alert(response.data.message);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
     
     window.location.href = "./login.html"
     // window.location.href = "./login.html"
@@ -34,26 +34,26 @@ function signup() {
 }
 
 function login() {
-    var email= (document.getElementById("email").value);
-     var password= (document.getElementById("password").value)
+    // var email= (document.getElementById("email").value);
+    //  var password= (document.getElementById("password").value)
     
-    // axios({
-    //     method: 'post',
-    //     url: "http://localhost:3000/login",
-    //     data: {
-    //         email: document.getElementById("email").value,
-    //         password: document.getElementById("password").value,
-    //     },
-    //     withCredentials: true
-    // })
-    //     .then(function (response) {
-    //         console.log(response.data.message);
-    //         alert(response.data.message);
+    axios({
+        method: 'post',
+        url: "http://localhost:3000/login",
+        data: {
+            email: document.getElementById("email").value,
+            password: document.getElementById("password").value,
+        },
+        
+    })
+        .then(function (response) {
+            console.log(response.data.message);
+            alert(response.data.message);
             
-    //     })
-    //     .catch(function (error) {
-    //         console.log(error);
-    //     });
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
         
         window.location.href = "profile.html";
     return false;
@@ -61,12 +61,12 @@ function login() {
 
 
 function profile() {
-    // axios.get('http://localhost:3000/profile').then(resp => {
-    //     document.getElementById("b-name").innerHTML = resp.data.profile.uname;
-    //     document.getElementById("b-email").innerHTML = resp.data.profile.email;
-    //     document.getElementById("b-phone").innerHTML = resp.data.profile.phone;
-    //     document.getElementById("b-gender").innerHTML = resp.data.profile.gender;
-    // });
+    axios.get('http://localhost:3000/profile').then(resp => {
+        document.getElementById("b-name").innerHTML = resp.data.profile.uname;
+        document.getElementById("b-email").innerHTML = resp.data.profile.email;
+        document.getElementById("b-phone").innerHTML = resp.data.profile.phone;
+        document.getElementById("b-gender").innerHTML = resp.data.profile.gender;
+    });
     window.location.href ="./forggten.html"
     return false;
 }
